92c226730d65ea31db397237cc4caabf46a6e87d
kellylandazuri <kellrocanlover@gmail.com>
2019-07-09 20:24:01 -0500

    Dynamo_GET,PUT
     LISTAR DOCUMENTO POR EMPRESA y ESTADO (activo/inactivo) 
    LISTAR PERMISOS POR DOCUMENTO
    ELIMINAR

--- DocManagement/api/controllers/docManagementController.js
+++ DocManagement/api/controllers/docManagementController.js
@@ -1,72 +1,79 @@
 //All control operations or functions are described here
 'use strict';
-var AWS = require('aws-sdk'), fs = require('fs'), model = require('../models/docManagementModel');
+var AWS = require('aws-sdk'),
+    fs = require('fs'),
+    model = require('../models/docManagementModel');
 var s3 = new AWS.S3();
 exports.helloWorld = function(req, res) {
-   res.send(process.env.G);
+    res.send(process.env.G);
 };
 
-exports.createFatherFolder = function(req,res){
-   var params = { 
-      Bucket: process.env.BUCKET, 
-      Key: req.body.companyId+"/"
-   };
-   s3.putObject(params, function(err, data) {
-      if (err){
-         console.log(err, err.stack);
-         res.json(err.stack);
-      } 
-      else{
-         console.log(data);
-         var dbData = req.body;
-         dbData.documentpath = dbData.companyId+"/";
-         dbData.versionId = data.VersionId;
-         dbData.name = dbData.companyId;
-         dbData.accessDate = (new Date()).toISOString();
-         dbData.modificationDate = (new Date()).toISOString();
-         dbData.creationDate = (new Date()).toISOString();
-         model.putDocumentFolderToDB(dbData,function(errDynamo,dataDynamo){
-            if(err){
-               res.json(errDynamo.stack);
-            }
-            else{
-               res.json(dataDynamo);
-            }
-         });       
-      }     
-   });
+exports.createFatherFolder = function(req, res) {
+    var params = {
+        Bucket: process.env.BUCKET,
+        Key: req.body.companyId + "/"
+    };
+    s3.putObject(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            res.json(err.stack);
+        } else {
+            console.log(data);
+            var dbData = req.body;
+            dbData.documentpath = dbData.companyId + "/";
+            dbData.versionId = data.VersionId;
+            dbData.name = dbData.companyId;
+            dbData.accessDate = (new Date()).toISOString();
+            dbData.modificationDate = (new Date()).toISOString();
+            dbData.creationDate = (new Date()).toISOString();
+            model.putDocumentFolderToDB(dbData, function(errDynamo, dataDynamo) {
+                if (err) {
+                    res.json(errDynamo.stack);
+                } else {
+                    res.json(dataDynamo);
+                }
+            });
+        }
+    });
 }
-exports.putDocumentFolder = function(req,res){
-   var params = {
-      Body:fs.readFileSync(req.files.document.path), 
-      Bucket: process.env.BUCKET, 
-      Key: req.fields.fileName
-   };
-   s3.upload(params, function(err, data) {
-      if (err){
-         console.log(err, err.stack);
-         res.json(err.stack);
-      } 
-      else{
-         console.log(data);
-         res.json(data);
-      }     
-   });
+exports.putDocumentFolder = function(req, res) {
+    var params = {
+        Body: fs.readFileSync(req.files.document.path),
+        Bucket: process.env.BUCKET,
+        Key: req.fields.fileName
+    };
+    s3.upload(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            res.json(err.stack);
+        } else {
+            console.log(data);
+            res.json(data);
+        }
+    });
 }
-exports.getDocumentFolder = function(req,res){
-   var params = {
-      Bucket: process.env.BUCKET, 
-      Key: req.params.path,
-      VersionId: req.params.versionId
-   };
-   res.attachment(req.params.path);
-   var fileStream = s3.getObject(params, function(err, data) {
-      if (err){
-         console.log(err, err.stack); 
-      } 
-      else{
-         console.log(data);
-      }
-   }).createReadStream();
-   fileStream.pipe(res);           
+exports.getDocumentFolder = function(req, res) {
+    var params = {
+        Bucket: process.env.BUCKET,
+        Key: req.params.path,
+        VersionId: req.params.versionId
+    };
+    res.attachment(req.params.path);
+    var fileStream = s3.getObject(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log(data);
+        }
+    }).createReadStream();
+    fileStream.pipe(res);
+}
+
+exports.putRemoveDocumentFolder = function(req, res) {
+    var params = {
+        Body: fs.readFileSync(req.files.document.path),
+        Bucket: process.env.BUCKET,
+        Key: req.fields.fileName
+    };
+
 }
--- DocManagement/api/models/docManagementModel.js
+++ DocManagement/api/models/docManagementModel.js
@@ -8,25 +8,25 @@ var docClient = new AWS2.DynamoDB.DocumentClient(new AWS2.Endpoint("https://dyna
 
 var table = process.env.DYNAMOTABLE;
 
-exports.putDocumentFolderToDB = function(data,callback){
+exports.putDocumentFolderToDB = function(data, callback) {
     var params = {
-        TableName:table,
-        Item:{
+        TableName: table,
+        Item: {
             "DOCUMENTPATH": data.documentpath,
-            "INFO":{
+            "INFO": {
                 "PATH": data.documentpath,
-                "CURRENT_VERSIONID":data.versionId,
+                "CURRENT_VERSIONID": data.versionId,
                 "CLIENT_COMPANYID": data.companyId,
-                "USERID":data.userId,
+                "USERID": data.userId,
                 "NAME": data.name,
-                "ACCESS_DATE": data.accessDate,//FOR DOWNLOADS
-                "MODIFICATION_DATE":data.modificationDate,//FOR UPDATES AND DELETES
-                "CREATION_DATE":data.creationDate,
-                "DELETED_DATE":data.deletedDate,
-                "SIZE":data.size,//SIZE IN BYTES AND ONLY FOR DOCUMENTS
-                "FORMAT":data.format,
-                "STATE":data.state,
-                "TYPE":data.type,//COULD BE DOCUMENT OR A FOLDER
+                "ACCESS_DATE": data.accessDate, //FOR DOWNLOADS
+                "MODIFICATION_DATE": data.modificationDate, //FOR UPDATES AND DELETES
+                "CREATION_DATE": data.creationDate,
+                "DELETED_DATE": data.deletedDate,
+                "SIZE": data.size, //SIZE IN BYTES AND ONLY FOR DOCUMENTS
+                "FORMAT": data.format,
+                "STATE": data.state,
+                "TYPE": data.type, //COULD BE DOCUMENT OR A FOLDER
                 "PATH_FATHER": data.pathFather,
                 "ACTION_HISTORY": data.actions
             }
@@ -35,10 +35,84 @@ exports.putDocumentFolderToDB = function(data,callback){
     docClient.put(params, function(err, data) {
         if (err) {
             console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-            callback(err,null);
+            callback(err, null);
         } else {
             console.log("Added item:", JSON.stringify(data, null, 2));
-            callback(null,data);
+            callback(null, data);
         }
     });
+}
+
+
+exports.getDocumentFolderByBussines = function(data, callback) {
+    var params = {
+        TableName: table,
+        KeyConditionExpression: "companyId = :bs",
+        ExpressionAttributeValues: {
+            ":bs": data.companyId
+        }
+    }
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(" -", item.NAME + ": " + item.state);
+            });
+        }
+    });
+}
+exports.getDocumentFolderByState = function(data, callback) {
+    var params = {
+        TableName: table,
+        KeyConditionExpression: "companyId = :bs and  state = :st ",
+        ExpressionAttributeValues: {
+            ":bs": data.companyId,
+            ":st": data.state
+        }
+    }
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+            data.Items.forEach(function(item) {
+                console.log(" -", item.NAME + ": " + item.state);
+            });
+        }
+    });
+}
+
+exports.getPermissionsByDocumentFolder = function(name, callback) {
+    var params = {
+        TableName: table,
+        ProjectionExpression: "PERMISSIONS",
+        KeyConditionExpression: "path=:name ",
+        ExpressionAttributeValues: {
+            "name": name
+        }
+    }
+    docClient.query(params, function(err, data) {
+        if (err) {
+            console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Query succeeded.");
+        }
+    });
+}
+
+exports.deleteDocumentFolder = function(name, state, callback) {
+    var params = {
+        TableName: table,
+        Key: {
+            path: name
+        },
+        UpdateExpression: "set STATE = :state",
+        ExpressionAttributeValues: {
+            ":state": state
+        },
+        ReturnValues: "UPDATED_NEW"
+    };
+    docClient.update(params, callback);
 }
